7f45b238912cd937a86a04b42ec0248aaa4db49e
Stephen Burke <sburke@one.verizon.com>
2016-04-24 21:08:34 -0400

    Alphabet

--- numbersystems/loop/main.go
+++ numbersystems/loop/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	for i := 65; i < 123; i++ {
+	for i := 65; i < 91; i++ {
+		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
+	}
+	for i := 97; i < 123; i++ {
 		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
 	}
 }
