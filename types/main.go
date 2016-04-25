2adf915c5497169d5be8b2de1b30d445be221032
Stephen Burke <sburke@one.verizon.com>
2016-04-24 20:33:14 -0400

    checking variable types

---
+++ types/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 99
+	b := 1.2
+	c := true
+	d := "My name is Stephen"
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+}
