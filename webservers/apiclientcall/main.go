d26afe41abdd8b292c251232ac4c0f1ebf0b5255
Stephen Burke <sburke@one.verizon.com>
2016-04-26 15:16:31 -0400

    Fixed formatting

--- webservers/apiclientcall/main.go
+++ webservers/apiclientcall/main.go
@@ -14,15 +14,12 @@ type respStruct struct {
 
 func main() {
 	api := gopencils.Api("http://localhost:9000")
-	// Users Resource
 	jobs := api.Res("jobs")
 
 	id := []string{"1"}
 
 	for _, username := range id {
-		// Create a new pointer to response struct
 		r := new(respStruct)
-		// Get user with id i into the newly created response struct
 		_, err := jobs.Id(username, r).Get()
 		if err != nil {
 			fmt.Println(err)
