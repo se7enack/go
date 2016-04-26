253288bcb988251a5997553530d9299c0089309e
Stephen Burke <sburke@one.verizon.com>
2016-04-26 14:46:19 -0400

    API Client and Server

---
+++ webservers/apiclientcall/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bndr/gopencils"
+)
+
+type respStruct struct {
+	Id    int
+	Name  string
+	Completed bool
+}
+
+func main() {
+	api := gopencils.Api("http://localhost:9000")
+	// Users Resource
+	jobs := api.Res("jobs")
+
+	id := []string{"1"}
+
+	for _, username := range id {
+		// Create a new pointer to response struct
+		r := new(respStruct)
+		// Get user with id i into the newly created response struct
+		_, err := jobs.Id(username, r).Get()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(r)
+		}
+	}
+}
